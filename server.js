582a5c9d0f5043b10a3e8040ec10f30310137074
Li Xuanji <xuanji@gmail.com>
2014-03-29 22:41:15 +0800

    switch to express.io, build basic single-page web app

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var express  = require('express');
+var express = require('express.io')
+var app = express();
 var fs = require('fs');
 
-var app  = express();
+app.http().io();
+
+app.listen(8080);
 
 app.configure(function() {
     app.use(express.static(__dirname + '/public'));         // set the static files location /public/img will be /img for users
@@ -10,12 +13,73 @@ app.configure(function() {
     app.use(express.methodOverride());                      // simulate DELETE and PUT
 });
 
-app.get('/description/:pid', function(req, res){
-    var filename = 'data/' + req.params.pid + '/description.html';
-    fs.readFile(filename, 'utf8', function(err, data){
-        res.send(data);
+function serve_json(path, res) {
+    res.set('Content-Type', 'application/json');
+    res.set('charset', 'utf-8');
+    res.charset = 'utf8';
+    fs.readFile(path, 'utf8', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.send('not-found');
+        } else {
+            res.send(data);            
+        }
     });
+}
+
+function serve_html(path, res) {
+    fs.readFile(path, 'utf8', function(err, data){
+        if (err) {
+            console.log(err);
+            res.send('404 Not Found'); //todo: actually set 404
+        } else {
+            res.send(data);
+        }
+    });
+}
+
+app.get('/', function(req, res) {
+    serve_html('index.html', res);
 });
 
-app.listen(8080);
-console.log("App listening on port 8080");
+app.get('/description/:pid', function(req, res){
+    var path = 'data/problems/' + req.params.pid + '/description.html';
+    serve_html(path, res);
+});
+
+app.get('/description/:pid/static/:static', function(req, res) {
+    res.send('not implemented yet ' + req.params.pid + req.params.static);
+});
+
+app.get('/problems/:cid', function(req, res) {
+    var path = 'data/contests/' + req.params.cid + '/problems.json';
+    serve_json(path, res);
+});
+
+app.get('/contestants/:cid', function(req, res) {
+    var path = 'data/contests/' + req.params.cid + 'contestants.json';
+    serve_json(path, res);
+});
+
+app.get('/contests/:uid', function(req, res) {
+    var path = 'data/users/' + req.params.uid + '.json';
+    serve_json(path, res);
+});
+
+app.io.route('submit', function(req) {
+    console.log(req.data);
+
+    var path = 'submissions/' + [req.data.cid, req.data.pid, req.data.uid].join('_');
+
+    fs.writeFile(path, req.data.source , function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    }); 
+
+    req.io.emit('talk', "hello");
+});
+
+//console.log("App listening on port 8080");
